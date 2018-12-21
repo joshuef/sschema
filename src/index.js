39b6212df75bb75d684e9548d9847a6fd5e09c9e
Josh Wilson <joshuef@gmail.com>
2018-12-21 11:55:56 +0000

    export the new schemas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import resolveableMap from '../src/resolveableMap.json';
+import resolveableItem from '../src/resolveableItem.json';
+import filesMap from '../src/filesMap.json';
+import fileItem from '../src/fileItem.json';
 import safeId from '../src/safeId.json';
 
 
 module.exports = {
+    fileItem,
+    filesMap,
+    resolveableItem,
     resolveableMap,
     safeId
 }
